9a1ccf098b9ba7852c59d91244f1bb9a2a68bb04
Julian <hellnerjulian@gmail.com>
2019-09-03 07:50:47 +0200

    ButtonHandler

---
+++ Inc/ButtonHandler.h
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ * Copyright (C) 2019 Julian Hellner - All Rights Reserved
+ * 
+ * The file ButtonHandler.h is part of Reflow.
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * Written by Julian Hellner <hellnerjulian@gmail.com>, Aug 27, 2019
+ *
+ ******************************************************************************/
+
+#ifndef BUTTONHANDLER_H_
+#define BUTTONHANDLER_H_
+
+class ButtonHandler {
+public:
+	ButtonHandler();
+};
+
+#endif /* BUTTONHANDLER_H_ */
---
+++ Src/ButtonHandler.cpp
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright (C) 2019 Julian Hellner - All Rights Reserved
+ * 
+ * The file ButtonHandler.cpp is part of Reflow.
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * Written by Julian Hellner <hellnerjulian@gmail.com>, Aug 27, 2019
+ *
+ ******************************************************************************/
+
+#include "ButtonHandler.h"
+
+ButtonHandler::ButtonHandler() {
+	// TODO Auto-generated constructor stub
+
+}
+
